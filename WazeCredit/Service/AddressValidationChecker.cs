3b9c0dc824b212e354bd361c8df04fdac6eca809
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-04-29 07:23:37 +0300

    Validation Checker Service

---
+++ WazeCredit/Service/AddressValidationChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public class AddressValidationChecker : IValidationChecker
+    {
+        public string ErrorMessage => "Location Validation failed";
+
+        public bool ValidatorLogic(CreditApplication model)
+        {
+            if (model.PostalCode <= 0 || model.PostalCode > 99999) return false;
+            return true;
+        }
+    }
+}
